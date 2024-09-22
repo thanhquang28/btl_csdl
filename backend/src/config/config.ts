1afa46602ca754156232daa162351db6f94fd69a
thanhquang28 <thanhquang2805@gmail.com>
2024-09-22 11:35:11 +0700

    feat: quest 1 query

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,3 +1,5 @@
 export const PORT = process.env.PORT || 3000;
 
 export const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/thanhlq';
+
+export const UNIT_PRICE = 500000 // 500k vnd / 1m2
--- backend/src/services/q1.ts
+++ backend/src/services/q1.ts
@@ -1,8 +1,88 @@
+import { UNIT_PRICE } from "../config/config"
 import CompanyModel from "../models/company.model"
 
 export const Quest1 = async () => {
-  const companies = await CompanyModel.find({})
-    .populate("towerAttributes.services")
-    .exec()
+  const companies = await CompanyModel.aggregate([
+    {
+      $unset: ["createdAt", "updatedAt", "employees"],
+    },
+    {
+      $lookup: {
+        from: "towerservicerecords",
+        let: { company: "$_id" },
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $eq: ["$company", "$$company"],
+              },
+            },
+          },
+          {
+            $group: {
+              _id: {
+                startDate: "$startDate",
+                endDate: "$endDate",
+              },
+              totalServiceBill: {
+                $sum: "$income",
+              },
+            },
+          },
+          {
+            $sort: {
+              "_id.startDate": 1,
+              "_id.endDate": 1,
+            },
+          },
+        ],
+        as: "serviceRecords",
+      },
+    },
+    {
+      $project: {
+        name: 1,
+        billByMonth: {
+          $map: {
+            input: "$serviceRecords",
+            as: "record",
+            in: {
+              startDate: "$$record._id.startDate",
+              endDate: "$$record._id.endDate",
+              totalServiceBill: "$$record.totalServiceBill",
+              rentBill: {
+                $multiply: ["$towerAttributes.size", UNIT_PRICE],
+              },
+              totalBill: {
+                $add: [
+                  "$$record.totalServiceBill",
+                  { $multiply: ["$towerAttributes.size", UNIT_PRICE] },
+                ],
+              },
+            },
+          },
+        },
+        totalBill: {
+          $sum: {
+            $map: {
+              input: "$serviceRecords",
+              as: "record",
+              in: {
+                $add: [
+                  "$$record.totalServiceBill",
+                  { $multiply: ["$towerAttributes.size", UNIT_PRICE] },
+                ],
+              },
+            },
+          },
+        },
+      },
+    },
+    {
+      $sort: {
+        totalBill: -1,
+      },
+    },
+  ])
   return companies
 }
